d866f3181044da150bbe6aed951e15694af2f408
svetikbaihe <166598440+svetikbaihe@users.noreply.github.com>
2024-09-03 21:29:24 +0900

    feat:avatar (#3)
    Co-authored-by: Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>

---
+++ src/components/Avatar/styles.module.scss.d.ts
@@ -0,0 +1,6 @@
+export declare const avatarWrapper: string;
+export declare const dateAvatar: string;
+export declare const iconUser: string;
+export declare const imgAvatar: string;
+export declare const infoWrapper: string;
+export declare const nameAvatar: string;
---
+++ src/components/Avatar/types.ts
@@ -0,0 +1,5 @@
+export interface AvatarProps {
+  img?: string,
+  name: string,
+  date: string
+}
