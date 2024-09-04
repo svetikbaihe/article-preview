e4778556f04e962d198d0488d1b742872b6e67b6
Світлана Ем <svitlanaem@MacBook-Air-Svitlana.local>
2024-09-04 22:37:41 +0900

    feat:typography

--- src/components/Avatar/component.tsx
+++ src/components/Avatar/component.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { AvatarProps } from "./types";
+import Typography from "@elements/Typography";
 import styles from "./styles.module.scss";
 import { FaCircleUser } from "react-icons/fa6";
 
-const Avatar: React.FC<AvatarProps> = ({ img = "", name = "", date = "" }) => {
+const Avatar: React.FC<AvatarProps> = ({ img, name, date}) => {
   return (
     <div className={styles.avatarWrapper}>
-      {img ? <img src={img} className={styles.imgAvatar}></img> : <FaCircleUser className={styles.iconUser}/>}
+      {img ? (
+        <img src={img} className={styles.imgAvatar}></img>
+      ) : (
+        <FaCircleUser className={styles.iconUser} />
+      )}
       <div className={styles.infoWrapper}>
-        <span className={styles.nameAvatar}>{name}</span>
-        <span className={styles.dateAvatar}>{date}</span>
+        <Typography
+          text={name}
+          type='overline'
+          color='dark-grayish'
+          weight='bold'
+          element='span'
+        />
+
+        <Typography
+          text={date}
+          type='overline'
+          color='light'
+          weight='medium'
+          element='span'
+        />
       </div>
     </div>
   );
---
+++ src/elements/Typography/styles.module.scss.d.ts
@@ -0,0 +1,8 @@
+export declare const textColorDarkGrayish: string;
+export declare const textColorGrayish: string;
+export declare const textColorLight: string;
+export declare const textSizeBody: string;
+export declare const textSizeOverline: string;
+export declare const textSizeSubtitle: string;
+export declare const textWeightBold: string;
+export declare const textWeightMedium: string;
